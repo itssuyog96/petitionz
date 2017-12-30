6cefdcf3917a32638a262795b566ece669649da7
Suyog Gadhave <itssuyog96@gmail.com>
2017-12-30 09:05:45 +0530

    Signup page init

--- web/index.php
+++ web/index.php
@@ -4,19 +4,21 @@ require('../vendor/autoload.php');
 
 // Using Medoo namespace
 use Medoo\Medoo;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 // Initialize
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app['db'] = new Medoo([
-  'database_type' => 'mysql',
-  'database_name' => 'q5r6zfzoeqsnvp5q',
-  'server' => 'vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  'username' => 'q7v7t2hzkho69580',
-  'password' => 'fifpiwbyfv1gc6xi'
-]);
+// $app['db'] = new Medoo([
+//   'database_type' => 'mysql',
+//   'database_name' => 'q5r6zfzoeqsnvp5q',
+//   'server' => 'vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//   'username' => 'q7v7t2hzkho69580',
+//   'password' => 'fifpiwbyfv1gc6xi'
+// ]);
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -33,10 +35,23 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
 
-  $data = $app['db']->select('aadhartbl', ['firstname', 'aadharno'], ['aadharno' => '123456789111']);
+  // $data = $app['db']->select('aadhartbl', ['firstname', 'aadharno'], ['aadharno' => '123456789111']);
 
   echo 'Fetched data : ' . json_encode($data);
   return $app['twig']->render('index.twig');
+});
+
+$app->get('/signup', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+
+  return $app['twig']->render('signup.twig');
+});
+
+$app->post('/register', function(Request $request) use($app) {
+  $app['monolog']->addDebug('logging output.');
+
+  echo $request->get('firstname') . " : " . $request->get('aadhar') . "\n";
+  return new Response('Values recieved', 200);
 });
 
 $app->run();
